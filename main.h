9620cd44c96f130e6a07f659fa39f3daf3455e45
ALSHOYA <hyamalsdyq2@gmail.com>
2023-10-11 12:50:48 +0300

    HOYAM

--- main.h
+++ main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#define _GNU_SOURCE
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,13 +10,16 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+extern char **environ;
 
 void rm_comment(char *command1);
 void rm_newline(char *line);
 void execute(char *line);
+char *_getenv(char *name);
 int _strlen(char *s);
 char *_strcpy(char *dest, char *src);
-void pwd(void);
+char *_strchr(char *s, char c);
+int _strcmp(char *s1, char *s2);
 void handle_path(char *command, char *path);
 void interactive(void);
 
